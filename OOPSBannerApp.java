ee2c99bfb42d31209387b4db86549219e9435510
Ultimate811 <gamingultimate811@gmail.com>
2026-02-24 13:09:10 +0530

    Initial Commit

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -11,5 +11,13 @@
 public class OOPSBannerApp {
 	public static void main(String[] args) {
 		System.out.println("OOPS");
+		// O           O           P            S
+        System.out.println(" ***  " + " " + " ***  " + " " + "*****  " + " " + " ***** ");
+        System.out.println("**  **" + " " + "**  **" + " " + "**  ** " + " " + "**     ");
+        System.out.println("**  **" + " " + "**  **" + " " + "**  ** " + " " + "**     ");
+        System.out.println("**  **" + " " + "**  **" + " " + "*****  " + " " + " ***** ");
+        System.out.println("**  **" + " " + "**  **" + " " + "**     " + " " + "    ** ");
+        System.out.println("**  **" + " " + "**  **" + " " + "**     " + " " + "    ** ");
+        System.out.println(" ***  " + " " + " ***  " + " " + "**     " + " " + " ***** ");
 	}
 }
